857535ad020daae049cf646d1de912408595c21b
thisistry <67001465+thisistry@users.noreply.github.com>
2020-07-01 15:50:26 +0530

    Update QDialog.py

--- QDialog.py
+++ QDialog.py
@@ -3,3 +3,11 @@ class MainForm(QDialog):
     def __init__(self, fn=None,parent=None):
         super(MainForm, self).__init__(parent,\
            flags=Qt.WindowMinimizeButtonHint|Qt.WindowMaximizeButtonHint)
+
+        
+        def closeEvent(self, evnt):
+        if self._want_to_close:
+            super(MyDialog, self).closeEvent(evnt)
+        else:
+            evnt.ignore()
+            self.setWindowState(QtCore.Qt.WindowMinimized)
