7680fed743b29f433e2d5cea6022fa6824a5f711
thisistry <67001465+thisistry@users.noreply.github.com>
2020-08-17 11:43:53 +0530

    Update jira.py

--- jira.py
+++ jira.py
@@ -10,3 +10,4 @@ jira=JIRA(options=jira_options,basic_auth=('admin','password'))
 issues_in_project = jira.search_issues('JQL',startAt=0, maxResults=2000, validate_query=True, fields=None, expand=None, json_result=None)
 
 print (issues_in_project)
+WARNING:root:Got recoverable error from GET http://alifsemi.atlassian.net/rest/api/2/serverInfo, will retry [1/3] in 19.5001791337s. Err: 401 Unauthorized
