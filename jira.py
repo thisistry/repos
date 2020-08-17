2f5bf6a89204e2d37448f3834350982e0cb6aa7e
thisistry <67001465+thisistry@users.noreply.github.com>
2020-08-17 12:53:26 +0530

    Update jira.py

--- jira.py
+++ jira.py
@@ -12,3 +12,20 @@ issues_in_project = jira.search_issues('JQL',startAt=0, maxResults=2000, validat
 print (issues_in_project)
 WARNING:root:Got recoverable error from GET http://alifsemi.atlassian.net/rest/api/2/serverInfo, will retry [1/3] in 19.5001791337s. Err: 401 Unauthorized
 AENp4e2PGrxvWqQSnb995262
+
+
+options = {'server': 'YOUR SERVER NAME'}
+jira = JIRA(options, basic_auth=('YOUR EMAIL', 'YOUR PASSWORD'))
+size = 100
+initial = 0
+while True:
+    start= initial*size
+    issues = jira.search_issues('project=<NAME OR ID>',  start,size)
+    if len(issues) == 0:
+        break
+    initial += 1
+    for issue in issues:
+        print 'ticket-no=',issue
+        print 'IssueType=',issue.fields.issuetype.name
+        print 'Status=',issue.fields.status.name
+        print 'Summary=',issue.fields.summary
